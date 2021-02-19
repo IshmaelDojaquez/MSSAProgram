89c32c48978175f0d84ff073715fa63ff9b2b483
IshmaelDojaquez <77464884+IshmaelDojaquez@users.noreply.github.com>
2021-02-18 21:58:54 -0500

    Circle Rectangle Square Triangle Classes

---
+++ Circle Rectangle Square Triangle Classes/Square.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle_Rectangle_Square_Triangle_Classes
+{
+    class Square
+    {
+        //Fields
+        private double side { get; set; }
+
+        //Constructors
+        public Square (double setSide)
+        {
+            SetSide(setSide);
+        }
+
+        //Methods
+        public double GetArea()
+        {
+            double area = Math.Pow(side, 2);
+            return area;
+        }
+
+        public double GetPerimeter()
+        {
+            double perimeter = 4 * side;
+            return perimeter;
+        }
+
+        public double GetSide()
+        {
+            return side;
+        }
+
+        public void SetSide(double setValue)
+        {
+            if (setValue <= 0)
+            {
+                setValue = 5;
+            }
+            side = setValue;
+        }
+    }
+}
