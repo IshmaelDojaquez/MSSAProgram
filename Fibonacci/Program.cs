2dfd60bff6f4800a58b49802a0c28a098b23469c
IshmaelDojaquez <77464884+IshmaelDojaquez@users.noreply.github.com>
2021-02-18 19:37:17 -0500

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fibonacci!!");
+            int i = 0;
+            int a = 0;
+            Console.WriteLine(a);
+            int b = 1;
+            Console.WriteLine(b);
+            int c = 0;
+
+            for (i = 0; i < 20; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
