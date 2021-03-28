b64eb323a39a4353176fe638626efc82d80143e7
Ishmael Dojaquez <ishmael.dojaquez@outlook.com>
2021-03-28 15:25:09 -0400

    Shapes [Using Inheritance from a Base Class]

---
+++ Shapes [Using Inheritance from a Base Class]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Hexagon(2);
+            Console.WriteLine(s.GetArea());
+            Console.WriteLine(s.GetPerimeter());
+        }
+    }
+}
